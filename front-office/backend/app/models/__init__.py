64cd9ac854e75671347be0881314ce5cd5fb93c5
Harena <salohyharentsoa00@gmail.com>
2026-09-12 09:23:25 +0300

    chore: suppression du dossier backend duplique dans front-office (relique obsolete, jamais utilisee)

--- front-office/backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/models/__init__.py
-from .concert import Concert
-from .ticket import Ticket
-
-# Add other models here if you have more
-
-__all__ = ['Concert', 'Ticket']  # List all model classes
